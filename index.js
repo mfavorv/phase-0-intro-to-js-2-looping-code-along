6b979ec66324433e21de69fd4beac9423d7f0cbf
Favor Vincent <vfavorm77@gmail.com>
2024-03-23 22:21:01 +0300

    JS looping code along lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "birthday";
+
+function writeCards(names, eventName) {
+  let surprise = [];
+  for (let c = 0; c < 3; c++) {
+    surprise.push(`Thank you, ${names[c]}, for the wonderful ${eventName} gift!`)
+  }
+return surprise;
+}
+
+function countDown(indexNumbers){
+    let indexNumber = 10;
+    while(indexNumber > -1){
+    console.log (indexNumber);
+    indexNumber = indexNumber -= 1;
+    }
+}
+
+
+
